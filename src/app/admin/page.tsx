5e055ff4d3f39e71d16c0dac1e0fda6e70e4cbe1
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-30 22:40:12 +0400

    component for our cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const getSalesData = () => {}
+
 const AdminDashboard = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <DashboardCard title='Sales' subtitle='Test' body='body' />
+    </div>
+  )
 }
 
 export default AdminDashboard
+
+type DashboardCardProps = {
+  title: string
+  subtitle: string
+  body: string
+}
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  )
+}
+
+// stopLine - 24:27
