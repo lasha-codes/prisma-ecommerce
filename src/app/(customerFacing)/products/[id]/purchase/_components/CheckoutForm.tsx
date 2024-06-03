437a06463c6ef27ad559ebc3a2ce3648f7b17711
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-06-03 23:38:57 +0400

    error handling in stripe and proccessing the checkout

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -17,6 +17,7 @@ import {
 } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import Image from 'next/image'
+import { FormEvent, useState } from 'react'
 
 type CheckoutFormProps = {
   product: {
@@ -68,21 +69,56 @@ export default CheckoutForm
 function Form({ priceInCents }: { priceInCents: number }) {
   const stripe = useStripe()
   const elements = useElements()
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string>()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (stripe === null || elements === null) {
+      return
+    }
+
+    setIsLoading(true)
+
+    // Check for existing order
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase/success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === 'card_error' || error.type === 'validation_error') {
+          setErrorMessage(error.message)
+        } else {
+          setErrorMessage('An unknown message occurred')
+        }
+      })
+      .finally(() => setIsLoading(false))
+  }
+
   return (
-    <form className='flex flex-col gap-4'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
       <PaymentElement />
       <Card>
         <CardHeader>
           <CardTitle>Checkout</CardTitle>
-          <CardDescription className='text-destructive'>Error</CardDescription>
+          <CardDescription className='text-destructive'>
+            {errorMessage}
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <Button
             className='w-full'
             size='lg'
-            disabled={stripe === null || elements === null}
+            disabled={stripe === null || elements === null || isLoading}
           >
-            Purchase - {formatCurrency(priceInCents / 100)}
+            {isLoading
+              ? 'Purchasing...'
+              : `Purchase - ${formatCurrency(priceInCents / 100)}`}
           </Button>
         </CardContent>
       </Card>
